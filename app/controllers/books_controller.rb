7385db59ad2f86dda7942f879472817ec560186c
shirotamaki <shiro.tamaki63@gmail.com>
2021-08-03 18:56:13 +0900

    並び順が一意になるorderを指定しました。title,idの順で指定しました。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    # @books = Book.all
-    @books = Book.page(params[:page]).per(2)
+    @books = Book.order(:title, :id).page(params[:page]).per(1)
   end
 
   # GET /books/1
