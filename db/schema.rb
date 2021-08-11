1d1ab44fbf3c63cebc56916263ad989191ec9868
shirotamaki <shiro.tamaki63@gmail.com>
2021-08-11 16:07:52 +0900

    エラーが起きたため、`rails db:migrate:reset`しやり直しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_053541) do
+ActiveRecord::Schema.define(version: 2021_08_11_065001) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,14 +19,6 @@ ActiveRecord::Schema.define(version: 2021_08_06_053541) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.string "picture"
-  end
-
-  create_table "ideas", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "picture"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
