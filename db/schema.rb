d7bbe98c7a07df7fd79f4ef0983e4a9b7b768488
shirotamaki <shiro.tamaki63@gmail.com>
2021-08-20 21:18:03 +0900

    I18n 国際化対応をしました

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2021_08_20_112948) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "postal_code"
+    t.string "address"
+    t.string "self_introduce"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
