72a165ceb8058f961779fff23f845f64a091b4d0
shirotamaki <shiro.tamaki63@gmail.com>
2021-08-30 15:21:41 +0900

    Rubocopに怒られた内容を修正しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def index
     @users = User.order(:email).page(params[:page]).per(2)
   end
+
   def show
     @user = if request.path == '/users/show'
               current_user
