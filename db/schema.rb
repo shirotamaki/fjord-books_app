924204f1a221035a3bf9dcecb98f59b004b38519
shirotamaki <shiro.tamaki63@gmail.com>
2021-09-04 14:01:40 +0900

    ルーティングを修正しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_112948) do
+ActiveRecord::Schema.define(version: 2021_09_04_041920) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
