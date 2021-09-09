340589cb6b5e2d3680222fe22f600522f3dd26b0
shirotamaki <shiro.tamaki63@gmail.com>
2021-09-09 21:28:52 +0900

    関連付けを行いました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :followed_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :followed_users, through: :followed_relationships, source: :followed
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: :followed_id
+  has_many :follower_users, through: :follower_relationships, source: :follower
 end
