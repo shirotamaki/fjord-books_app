68b1a86ef31c7c1c2885737de815b19cde8e3e4c
shirotamaki <shiro.tamaki63@gmail.com>
2021-09-25 20:34:17 +0900

    各コントローラーを作成し、アクションメソッドを定義しました。

---
+++ app/controllers/following_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class FollowingController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    @following = user.following
+  end
+end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,13 @@
 
 class RelationshipsController < ApplicationController
   def create
+    user = User.find(params[:followed_id])
+    current_user.follow(params[:followed_id])
+    redirect_to user_path(user)
   end
+
   def destroy
+    current_user.unfollow(params[:id])
+    redirect_to user_path(params[:id])
   end
 end
