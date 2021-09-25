908f100043091ab989f4c8894f8081d563aac92b
shirotamaki <shiro.tamaki63@gmail.com>
2021-09-25 21:02:39 +0900

    inverse_of: の値（指定内容）を修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: :user
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: 'follower'
   has_many :following, through: :active_relationships, source: :followed
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy, inverse_of: :user
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy, inverse_of: 'followed'
   has_many :followers, through: :passive_relationships, source: :follower
 
   def follow(other_user)
