f7d20fa5cf4a2af2a0c0820e747d68bf9b053c3a
shirotamaki <shiro.tamaki63@gmail.com>
2021-09-25 20:27:08 +0900

    アソシエーション（関連付け）を行いました。

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
+# frozen_string_literal: true
+
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
 end
-
-# => ["id", "follower_id", "followed_id", "created_at", "updated_at"]
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,31 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+                                    :recoverable,
+                                    :rememberable,
+                                    :validatable
 
   has_one_attached :avatar
-  has_many :followed_relationships, class_name: 'Relationship', foreign_key: :follower_id
-  has_many :followed_users, through: :followed_relationships, source: :followed
-  has_many :follower_relationships, class_name: 'Relationship', foreign_key: :followed_id
-  has_many :follower_users, through: :follower_relationships, source: :follower
+  has_many :active_relationships, class_name: 'Relationship',
+                                 foreign_key: :follower_id,
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships,
+                        source: :followed
+  has_many :passive_relationships, class_name: 'Relationship',
+                                  foreign_key: :followed_id,
+                                    dependent: :destroy
+  has_many :followers, through: :passive_relationships,
+                        source: :follower
 
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
-
