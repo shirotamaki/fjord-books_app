a4441dfc07458d1e15f6a8d99d39761207d77c72
shirotamaki <shiro.tamaki63@gmail.com>
2021-09-26 13:54:50 +0900

    引数名を変更しました。

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@
 class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
-    current_user.follow(params[:followed_id])
+    current_user.follow(user.id)
     redirect_to user_path(user)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy, inverse_of: 'followed'
   has_many :followers, through: :passive_relationships, source: :follower
 
-  def follow(other_user)
-    active_relationships.create(followed_id: other_user)
+  def follow(other_user_id)
+    active_relationships.create(followed_id: other_user_id)
   end
 
-  def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user).destroy
+  def unfollow(other_user_id)
+    active_relationships.find_by(followed_id: other_user_id).destroy
   end
 
-  def following?(other_user)
-    following.include?(other_user)
+  def following?(other_user_id)
+    following.include?(other_user_id)
   end
 end
