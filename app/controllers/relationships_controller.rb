645e07b94eccb83a0bb09316b03a23956308b9eb
shirotamaki <shiro.tamaki63@gmail.com>
2021-10-03 10:50:03 +0900

    followメソッドの内容をシンプルに修正しました。

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@
 class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
-    current_user.follow(user.id)
+    current_user.follow(user)
     redirect_to user_path(user)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: :followed_id, dependent: :destroy, inverse_of: 'followed'
   has_many :followers, through: :passive_relationships, source: :follower
 
-  def follow(other_user_id)
-    active_relationships.create(followed_id: other_user_id)
+  def follow(other_user)
+    following << other_user
   end
 
   def unfollow(other_user_id)
     active_relationships.find_by(followed_id: other_user_id).destroy
   end
 
-  def following?(other_user_id)
-    following.include?(other_user_id)
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
