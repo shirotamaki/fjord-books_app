e56ad76ba955687babfbb8a6d3cb9b2114a601fa
shirotamaki <shiro.tamaki63@gmail.com>
2022-01-25 17:32:58 +0900

    Relationshipモデルを準備し関連付けを行う

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  # 能動的：フォローする
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 特定のユーザーがフォローしているユーザーを取得できるメソッド
+  # @user.active_relationships.map(&:followed) =>（ @userがフォローしているユーザーをmapで取得）
+  has_many :following, through: :active_relationships, source: :followed
+
+  # 受動的：フォローされる
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 end
