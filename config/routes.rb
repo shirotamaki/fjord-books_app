07cf118a0ff38b51a212af67c86f445e68eecef9
shirotamaki <shiro.tamaki63@gmail.com>
2022-01-26 18:41:28 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'relationships/following'
+  get 'relationships/followers'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
-  resources :users, only: %i(index show)
+  resources :users, only: %i(index show) do
+    member do
+      get :following, :followers
+    end
+  end
 end
+
