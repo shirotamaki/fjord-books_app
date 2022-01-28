de4ad2011c798dac8864e7435090f0e152d55af6
shirotamaki <shiro.tamaki63@gmail.com>
2022-01-28 18:52:01 +0900

    `rails db:migrate:reset`をし、`rails db:seed`コマンドで初期データを投入

--- db/schema.rb
+++ db/schema.rb
@@ -59,16 +59,6 @@ ActiveRecord::Schema.define(version: 2022_01_28_080833) do
     t.index ["follower_id"], name: "index_friendships_on_follower_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
