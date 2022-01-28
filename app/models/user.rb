8176c2c3a48c5e6d261e92ab4b02949fa6294118
shirotamaki <shiro.tamaki63@gmail.com>
2022-01-28 18:25:02 +0900

    コメントを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,27 +6,20 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  # 能動的：フォローする
   has_many :active_friendships, class_name: 'Friendship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: 'follower'
-  # 特定のユーザーがフォローしているユーザーを取得できるメソッド
-  # @user.active_friendships.map(&:followed) =>（ @userがフォローしているユーザーをmapで取得）
   has_many :following, through: :active_friendships, source: :followed
 
-  # 受動的：フォローされる
   has_many :passive_friendships, class_name: 'Friendship', foreign_key: 'followed_id', dependent: :destroy, inverse_of: 'followed'
   has_many :followers, through: :passive_friendships, source: :follower
 
-  # ユーザーをフォローする
   def follow(other_user)
     following << other_user
   end
 
-  # ユーザーをフォロー解除する
   def unfollow(other_user)
     active_friendships.find_by(followed_id: other_user.id).destroy
   end
 
-  # 現在のユーザーがフォローしていたらtrueを返す
   def following?(other_user)
     following.include?(other_user)
   end
