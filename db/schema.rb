5be37374e8859216f320a1fc0b2b261d9792db0d
shirotamaki <shiro.tamaki63@gmail.com>
2022-02-02 20:57:53 +0900

    commentsテーブルへuser_idカラムを追加。usersテーブルと紐づける

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_095729) do
+ActiveRecord::Schema.define(version: 2022_02_02_115224) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_095729) do
     t.integer "commentable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
   end
 
