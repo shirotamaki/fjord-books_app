e029fd2a4368d70355faaa8469cc0dcd0a2e2ec0
shirotamaki <shiro.tamaki63@gmail.com>
2022-02-09 21:31:51 +0900

    投稿ユーザーのnameとemailを表示

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,6 +9,7 @@ class ReportsController < ApplicationController
   # GET /reports/1 or /reports/1.json
   def show
     @report = Report.find(params[:id])
+    @user = User.all
   end
 
   # GET /reports/new
