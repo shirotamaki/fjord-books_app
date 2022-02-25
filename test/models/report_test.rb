f56fddce2a01eb44a014ad449355cd49c6e1c6d1
shirotamaki <shiro.tamaki63@gmail.com>
2022-02-25 22:20:43 +0900

    Test: モデルテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(name: 'Alice', email: 'alice@example.com', password: 'password')
+    @today = Time.current
+    @report = Report.new(user: @user, title: 'Alice\'s memo', content: 'Alice\'s content', created_at: @today)
+  end
+
+  test '#editable?' do
+    assert @report.editable?(@user)
+  end
+
+  test '#created_on' do
+    assert_equal @today.to_date, @report.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = User.create!(email: 'alice@example.com', password: 'password')
+    @bob = User.create!(email: 'bob@example.com', password: 'password')
+    @relationship = Relationship.new(following: @alice, follower: @bob)
+  end
+
+  test "#name_or_email" do
+    user = User.new(email: 'alice@example.com', name: '')
+    assert_equal 'alice@example.com', user.name_or_email
+
+    user.name = 'Alice'
+    assert_equal 'Alice', user.name_or_email
+  end
+
+  test '#following?' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+  end
+
+  test 'followed_by?' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @bob.followed_by?(@alice)
+  end
+
+  test '#follow' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @bob.followers.include?(@alice)
+  end
+
+  test '#unfollow' do
+    @alice.follow(@bob)
+    assert @bob.followers.include?(@alice)
+    @alice.unfollow(@bob)
+    assert_not @bob.followers.include?(@alice)
+  end
 end
